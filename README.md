06dad936a68cb8abf4be1ee016e6d35d60b053e7
baaki20 <huduabdulbaakijunior20@gmail.com>
2025-10-08 05:33:29 +0000

    add contains duplicate

---
+++ README.md
@@ -0,0 +1,5 @@
+There is a world where 150 days from now we would be confident in facing any DSA question and one where DSA still dey scare us.
+
+The outcome depends on our commitment for the next 150 days
+
+Goodluck 🍻
---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums){
+        Set<Integer> uniqueNums = new HashSet<>();
+
+        // A more concise way to do this is:
+        // we always have to loop through the whole array before we can conclude on the result
+        /*
+        for(int num: nums){
+            uniqueNums.add(num);
+        }
+        return nums.length != uniqueNums.size();
+        */
+
+
+
+        // A more concise way to do this is:
+        // we can return true as soon as we find a duplicate
+        for(int num: nums){
+            if(uniqueNums.contains(num)){
+                return true;
+            }
+            uniqueNums.add(num);
+        }
+        return false;
+    }
+}
